5adb41b1446a4fb06acd141345546f813b479fff
Mochammad Iqbal <qblmchmmd.dev@gmail.com>
2018-11-03 16:28:28 +0700

    Change dev and prod configuration

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -6,49 +6,51 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin')
 
-module.exports = {
-  stats: 'none',
-  entry: './src/main.js',
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.vue$/,
-        loader: 'vue-loader'
-      },
-      {
-        test: /\.pug$/,
-        loader: 'pug-plain-loader'
-      },
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          use: 'css-loader',
-          fallback: ['vue-style-loader', 'style-loader'],
-          allChunks: true
-        })
-      },
-      {
-        enforce: 'pre',
-        test: /\.(js|vue)$/,
-        exclude: /node_modules/,
-        loader: 'eslint-loader',
-        options: {
-          failOnWarning: true
+module.exports = env => {
+  return {
+    stats: 'none',
+    entry: './src/main.js',
+    output: {
+      path: path.resolve(__dirname, env === 'production' ? 'build' : 'dist'),
+      filename: 'bundle.js'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.vue$/,
+          loader: 'vue-loader'
+        },
+        {
+          test: /\.pug$/,
+          loader: 'pug-plain-loader'
+        },
+        {
+          test: /\.css$/,
+          use: ExtractTextPlugin.extract({
+            use: 'css-loader',
+            fallback: ['vue-style-loader', 'style-loader'],
+            allChunks: true
+          })
+        },
+        {
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          exclude: /node_modules/,
+          loader: 'eslint-loader',
+          options: {
+            failOnWarning: true
+          }
         }
-      }
+      ]
+    },
+    plugins: [
+      // make sure to include the plugin for the magic
+      new VueLoaderPlugin(),
+      new HtmlWebpackPlugin({
+        template: './index.html'
+      }),
+      new ExtractTextPlugin('styles.css'),
+      new FriendlyErrorsWebpackPlugin()
     ]
-  },
-  plugins: [
-    // make sure to include the plugin for the magic
-    new VueLoaderPlugin(),
-    new HtmlWebpackPlugin({
-      template: './index.html'
-    }),
-    new ExtractTextPlugin('styles.css'),
-    new FriendlyErrorsWebpackPlugin()
-  ]
+  }
 }
